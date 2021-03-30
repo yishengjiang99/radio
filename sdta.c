88dca36585ee7f60cb949dbc80525a4cfd476b22
Yisheng Jiang <yishengjiang@yahoo.com>
2021-03-29 19:20:51 -0700

    afdfda

--- sdta.c
+++ sdta.c
@@ -1,24 +1,94 @@
+#define DEBUG 1
+#ifdef DEBUG
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#endif
+
+int load(int n, void *data, float *floats);
+int render(float *output, int position, int startLoop, int endLoop, float ratio, float multiplier);
+
 static int nsamples;
-short *bit16s;
-float *floats;
-int load(void *data, float *floats)
+static short *bit16s;
+static float *floats;
+
+#ifdef DEBUG
+int main()
 {
+  char ob[1024];
+  int start = 2426 + 4;
+  int end = 31028642;
+
+  FILE *fd = fopen("./sf2/GeneralUserGS.sf2", "rb");
+  fseek(fd, start, SEEK_SET);
+  uint8_t *fob = (uint8_t *)malloc(end - start + 1);
+
+  fread(fob, 1, start - end, fd);
+  floats = (float *)malloc(sizeof(float) * (end - start));
+  load(end - start, fob, floats);
+  float *output = (float *)malloc(sizeof(float) * 128);
+  int n = 48000;
+  int position = 5578359;
+  FILE *ffp = popen("ffplay -f f32le -i pipe:0 -ac  1 -ar 48k", "w");
+  while (n > 128)
+  {
+    float *ptr = output;
+
+    position = render(ptr, position, 5580605, 5587681, 1 / 4.0f, 1);
+    fwrite(output, sizeof(float), 128, ffp);
+    n = n - 128;
+    int usleep(useconds_t usec);
+    usleep(340);
+  }
+
+  pclose(ffp);
+}
+#endif
+
+int load(int n, void *data, float *floats)
+{
+  nsamples = n;
   bit16s = (short *)data;
   data = data + nsamples * sizeof(short);
   for (int i = 0; i < nsamples; i++)
   {
-    *(floats + i) = *(bit16s + i) * 1.0f / 0x8000;
+    *(floats + i) = *(bit16s + i) * 1.0f / 0xffff;
   }
+  return 1;
 }
-typedef struct
-{
-  int start, end, loopStart, loopEnd
-} voice;
 
-void voiceOn()
+int render(float *output, int position, int startLoop, int endLoop, float ratio, float multiplier)
 {
+
+  int loopr = (endLoop - startLoop);
+  float shift = 0.0f;
+
+  for (int i = 0; i < 128; i++)
+  {
+    *(output + i) = *(output + i) + *(floats + position);
+
+    shift += ratio;
+
+    while (shift >= 1)
+    {
+      shift--;
+      position++;
+    }
+    if (position >= endLoop)
+    {
+      position -= loopr;
+    }
+    // printf("\n%d", position);
+  }
+  return position;
 }
-void render(int block, voice *voices)
+float hermite4(float frac_pos, float xm1, float x0, float x1, float x2)
 {
-  float* output = malloc(voices)
+  const float c = (x1 - xm1) * 0.5f;
+  const float v = x0 - x1;
+  const float w = c + v;
+  const float a = w + v + (x2 - x0) * 0.5f;
+  const float b_neg = w + a;
+
+  return ((((a * frac_pos) - b_neg) * frac_pos + c) * frac_pos + x0);
 }
